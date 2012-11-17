d1d9ea3c490a809172f3cdc0efb95dba22ac2b4b
Feynman Liang <feynman.liang@gmail.com>
2012-11-17 13:33:55 -0500

    Add databse config to gitignore

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,32 @@
 class LocationsController < ApplicationController
   respond_to :html, :xml, :json
+  validate_presence_of :title, :lat, :long
 
   def index
     @locations = Location.all
+
     respond_with(@locations)
+  end
+
+  def show
+
+  end
+
+  def new
+    @location = Location.new
+
+    respond_with(@location)
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
