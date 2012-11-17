3c7510ec229083f67beb6372dd79b7555c91a325
Feynman Liang <feynman.liang@gmail.com>
2012-11-17 13:53:12 -0500

    Add loation new action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,25 +1,43 @@
 class LocationsController < ApplicationController
-  respond_to :html, :xml, :json
-  validate_presence_of :title, :lat, :long
-
   def index
     @locations = Location.all
 
-    respond_with(@locations)
+    respond_to do |format|
+      format.html
+      format.json { render json: @locations }
+    end
   end
 
   def show
+    @location = Location.find(params[:id])
 
+    respond_to do |format|
+      format.html
+      format.json { render json: @location }
+    end
   end
 
   def new
     @location = Location.new
 
-    respond_with(@location)
+    respond_to do |format|
+      format.html
+      format.json { render json: @locations }
+    end
   end
 
   def create
+    @location = Location.new(params[:location])
 
+    respond_to do |format|
+      if @location.valid? && @location.save
+        format.html { redirect_to @location, notice: 'Location was successfully created.' }
+        format.json { render json: @location, status: :created, location: @location }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,8 @@
 class Location < ActiveRecord::Base
-  attr_accessible :description, :lat, :long, :title
+  #attr_accessible :description, :lat, :long, :title
+
+  validate :title, :presence => true
+  validate :lat, :long, { :presence => true, :numericality => true }
+
+  default_scope order: 'locations.created_at DESC'
 end
