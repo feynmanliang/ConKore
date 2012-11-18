b9ca071f15f39a05280ff40b3276180dfac876e5
Feynman Liang <feynman.liang@gmail.com>
2012-11-18 04:31:20 -0500

    Shorted seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -162,7 +162,9 @@ tricks.each do |trick|
       user_id: 1+rand(users.count),
       trick_id: trick.id,
       rating: 1+rand(10),
-      body: "You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out."
+      body: "You think water moves fast? You should see ice. It moves like it
+      has a mind. Like it knows it killed the world once and got a taste for
+      murder. "
     })
   end
 end
